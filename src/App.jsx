c1621bdbd2a20013df266cbdbaa49f41437ad340
Alexandre Junior <alejunior.dev@gmail.com>
2025-02-15 15:10:04 -0300

    feat: create Tasks component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,51 @@
+import { useState } from "react";
 import AddTask from "./components/AddTask";
 import Tasks from "./components/Tasks";
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      title: "Estudar React",
+      description: "Estudar React para criar aplicações web",
+      isCompleted: false,
+    },
+    {
+      id: 2,
+      title: "Estudar Tailwind CSS",
+      description: "Estudar Tailwind CSS para estilizar aplicações web",
+      isCompleted: false,
+    },
+    {
+      id: 3,
+      title: "Estudar TypeScript",
+      description:
+        "Estudar TypeScript para adicionar tipagem em aplicações web",
+      isCompleted: false,
+    },
+  ]);
+
+  function onRemoveTask(taskId) {
+    const newTasks = tasks.filter((task) => task.id !== taskId);
+    console.log(newTasks);
+    setTasks(newTasks);
+  }
+
+  function onTaskClick(taskId) {
+    const newTasks = tasks.map((task) => {
+      // Preciso atualizar essa tarefa
+      if (task.id === taskId) {
+        return {
+          ...task,
+          isCompleted: !task.isCompleted,
+        };
+      }
+      // Não preciso atualizar essa tarefa
+      return task;
+    });
+    setTasks(newTasks);
+  }
+
   return (
     <div className="flex h-screen w-screen justify-center bg-slate-500 p-6">
       <div className="w-[500px]">
@@ -10,7 +54,7 @@ function App() {
         </h1>
 
         <AddTask />
-        <Tasks />
+        <Tasks {...{ tasks, onTaskClick, onRemoveTask }} />
       </div>
     </div>
   );
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,5 +1,31 @@
-function Tasks() {
-  return <h1>tasks</h1>;
+import { ChevronRightIcon, TrashIcon } from "lucide-react";
+
+function Tasks({ tasks, onTaskClick, onRemoveTask }) {
+  return (
+    <ul className="space-y-4 rounded-md bg-slate-200 p-6 shadow">
+      {tasks.map((task) => (
+        <li key={task.id} className="flex gap-2">
+          <button
+            onClick={() => onTaskClick(task.id)}
+            className={`w-full rounded-md bg-slate-400 p-2 text-left text-white ${task.isCompleted && "line-through"}`}
+          >
+            {task.title}
+          </button>
+
+          <button className="rounded-md bg-slate-400 p-2 text-white">
+            <ChevronRightIcon />
+          </button>
+
+          <button
+            onClick={() => onRemoveTask(task.id)}
+            className="rounded-md bg-slate-400 p-2 text-white"
+          >
+            <TrashIcon />
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
 }
 
 export default Tasks;
